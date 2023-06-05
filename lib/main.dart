1b4f3c2adc264b012ddb9817db1eea2f1d217618
jimibilibob <jimibilibob@hotmail.com>
2023-06-05 00:37:26 -0400

    Add general screen design. (#6)
    Co-authored-by: jimibilibob <isra.neri2@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:schoo_parents/config/themes/salle_cbba_theme.dart';
 import 'package:schoo_parents/presentation/screens/home/home_screen.dart';
 import 'package:schoo_parents/presentation/screens/student-home/student_home_screen.dart';
+import 'package:schoo_parents/presentation/statistics/general_statistic_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: 'Yes No App',
         theme: SalleCbbaTheme.theme(),
-        home: const StudentHomeScreen()
+        home: const GeneralStatisticScreen()
       );
   }
 }
